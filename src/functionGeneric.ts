27977797ddb88088412ad3499dc18f869dc7622c
Md Rifat Hossen <rifatsaown0@gmail.com>
2024-05-05 00:40:49 +0600

    feat: Add generic function for creating arrays with tuples
    This commit adds a new generic function, `createArrayWithTuple`, to the `functionGeneric.ts` file. The function takes two parameters of different types and returns an array containing those parameters as a tuple.
    
    The commit also includes examples of using the `createArrayWithTuple` function to create tuples of strings and numbers, as well as tuples of numbers and objects with name and age properties.
    
    Refactor: Update user variable to use IPerson interface in genericTypes.ts

--- src/functionGeneric.ts
+++ src/functionGeneric.ts
@@ -14,3 +14,27 @@ const resGenericObj = createArrayWithGeneric<{
 }>({
     name:"Bangladesh", age:50
 })
+
+
+const createArrayWithTuple = <X,Y>(param1:X, param2:Y): [X,Y] => {
+    return [param1, param2]
+}
+
+const resTuple = createArrayWithTuple<string, number>("Bangladesh", 50)
+const resTupleObj = createArrayWithTuple<number, {name:string, age:number}>(50, {name:"Bangladesh", age:50})
+
+
+
+
+const addCourseToStudent = <T>(student:T, course:string): T & {course:string} => {
+    return {
+        ...student,
+        course
+    }
+}
+
+const student1 = addCourseToStudent<{name:string,age:number,email?:string}>({
+    name:"Elie",
+    age:25,
+    email: "test@g.com"
+}, "Math")
