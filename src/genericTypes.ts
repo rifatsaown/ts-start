eaa18135cd5c6667542d290e6312e2b232082e11
Md Rifat Hossen <rifatsaown0@gmail.com>
2024-05-05 00:11:30 +0600

    refactor: Update genericTypes.ts to use Array<T> instead of T[]
    This commit updates the code in genericTypes.ts to use the Array<T> syntax instead of the T[] syntax for defining arrays. The change is made consistently throughout the file for the rollNumber and mentors variables.
    
    It also introduces the GenericType<T> type alias and demonstrates its usage with number and string types. Additionally, it defines the GenericTuple<X,Y> type for creating tuples with generic types.
    
    Lastly, it adds the export {} statement at the end of the file to prevent variable redeclaration errors.

---
+++ src/genericTypes.ts
@@ -0,0 +1,26 @@
+// const rollNumber: number[] = [3, 4, 5];
+const rollNumber: Array<number> = [3, 4, 5];
+
+// const mentors: string[] = ['Elie', 'Matt', 'Joel'];
+const mentors: Array<string> = ['Elie', 'Matt', 'Joel'];
+
+type GenericType<T> = Array<T>;// Generic type
+
+const rollNumber2: GenericType<number> = [3, 4, 5];
+const mentors2: GenericType<string> = ['Elie', 'Matt', 'Joel'];
+
+
+const user: GenericType<object> = [
+    { name: 'Elie', age: 25 },
+    { name: 'Matt', age: 35 },
+    { name: 'Joel', age: 30 },
+]
+
+
+type GenericTuple<X,Y> = [X,Y] // Generic Tuple
+
+const manus:GenericTuple<string,string> = ["Mr. X", "Mr. Y"]
+const userWithId:GenericTuple<number,{name:string,age:number}> = [1, { name: 'Elie', age: 25 }]
+
+
+export { }; // To prevent error of variable redeclaration
