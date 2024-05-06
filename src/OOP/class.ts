e448f1fd292c047449aab022540009f0b811c06c
Md Rifat Hossen <rifatsaown0@gmail.com>
2024-05-06 18:13:05 +0600

    feat: Add class for Animal with makeSound method
    This commit adds a new file, `class.ts`, which contains a class definition for `Animal`. The `Animal` class has properties for `name`, `species`, and `sound`, and a method `makeSound` that logs the sound the animal makes to the console.
    
    The commit also includes an example of creating an instance of `Animal` with the name "Dog", species "Canis lupus", and sound "Bark", and calling the `makeSound` method on the instance.
    
    Refactor: Remove unused import statements in class.ts

---
+++ src/OOP/class.ts
@@ -0,0 +1,10 @@
+class Animal {
+    constructor(public name: string, public species: string, public sound: string){}
+
+    makeSound() {
+        console.log(`${this.name} says ${this.sound}`);
+    }
+}
+
+const dog = new Animal('Dog', 'Canis lupus', 'Bark');
+dog.makeSound(); 
